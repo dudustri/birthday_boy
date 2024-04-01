a37a64465768d00827ed40772ed4c7a636fb7794
dudustri <eduardostrindade@gmail.com>
2024-04-01 09:06:38 +0200

    fix: happy birthday message responsive position

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -5,55 +5,58 @@ import { styles } from "../../styles.js";
 import { textVariant } from "./animation.js";
 
 const Entry: React.FC = () => (
-	<section className="page-pattern relative w-full h-[100vh] mx-auto">
-		<div className="flex h-3/5 justify-center items-center">
-			<img
-				src="/team/birthdayboy.jpg"
-				alt="Profile"
-				className="w-[256px] h-[256px] rounded-full object-cover border-2 border-white"
-			/>
-		</div>
-		<div className="flex justify-center items-center text-center mt-[-70px] sm:mt-[-44px] md:mt-[-38px] lg:mt-[-34px] xl:mt-[-30px] 2xl:mt-[-24px]">
-			<div className="rounded-full bg-opacity-70 bg-black pr-8 pl-8 pt-6 pb-6 sm:pr-12 sm:pl-12 lg:pr-16 lg:pl-16 xl:pr-20 xl:pl-20 2xl:pr-24 2xl:pl-24">
-				<h1 className={styles.pageHeadText}>
-					<span className="text-cyan-300 text-xl max-w-full sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
-						<Typewriter
-							options={{
-								strings: [
-									"Hello Jorge!",
-									"Oi Jorginho?",
-									"Hola Jorgito uaaaaaa ~",
-									"Arrombado do caralho",
-									"Terror de la España",
-									"El pistolero de Mallorca",
-									"Hijo de puta",
-									"Chimarrão lover",
-								],
-								autoStart: true,
-								loop: true,
-							}}
-						/>
-					</span>
-				</h1>
-
-				<motion.div
-					variants={textVariant(0.35)}
-					initial="hidden"
-					whileInView="show"
-				>
-					<p
-						className={`mt-8 text-white text-[10px] sm:text-base md:text-lg lg:text-lg xl:max-w-4xl xl:text-xl 2xl:text-xl`}
-					>
-						Feliz cumpleaños, Jorge! 🎉 We built a birthday website for you,
-						cunt, to celebrate your life journey under the digital and cloudy
-						sky of Denmark. It is a reminder that age is just a number which
-						keeps increasing like entropy and your fucking dark humour with each
-						passing year!😨 Cheers buddy 🥂🍾💫
-					</p>
-				</motion.div>
+	<div>
+		<section className="page-pattern relative w-full h-[100vh] mx-auto">
+			<div className="flex h-3/5 justify-center items-center">
+				<img
+					src="/team/birthdayboy.jpg"
+					alt="Profile"
+					className="w-[256px] h-[256px] rounded-full object-cover border-2 border-white"
+				/>
 			</div>
-		</div>
-	</section>
+			<div className="flex justify-center items-center text-center mt-[-70px] sm:mt-[-44px] md:mt-[-38px] lg:mt-[-34px] xl:mt-[-30px] 2xl:mt-[-24px]">
+				<div className="rounded-full bg-opacity-70 bg-black pr-8 pl-8 pt-6 pb-6 sm:pr-12 sm:pl-12 lg:pr-16 lg:pl-16 xl:pr-20 xl:pl-20 2xl:pr-24 2xl:pl-24">
+					<h1 className={styles.pageHeadText}>
+						<span className="text-cyan-300 text-xl max-w-full sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+							<Typewriter
+								options={{
+									strings: [
+										"Hello Jorge!",
+										"Oi Jorginho?",
+										"Hola Jorgito uaaaaaa ~",
+										"Arrombado do caralho",
+										"Terror de la España",
+										"El pistolero de Mallorca",
+										"Hijo de puta",
+										"Chimarrão lover",
+									],
+									autoStart: true,
+									loop: true,
+								}}
+							/>
+						</span>
+					</h1>
+
+					<motion.div
+						variants={textVariant(0.35)}
+						initial="hidden"
+						whileInView="show"
+					>
+						<p
+							className={`mt-8 text-white text-[10px] sm:text-base md:text-lg lg:text-lg xl:max-w-4xl xl:text-xl 2xl:text-xl`}
+						>
+							Feliz cumpleaños, Jorge! 🎉 We built a birthday website for you,
+							cunt, to celebrate your life journey under the digital and cloudy
+							sky of Denmark. It is a reminder that age is just a number which
+							keeps increasing like entropy and your fucking dark humour with
+							each passing year!😨 Cheers buddy 🥂🍾💫
+						</p>
+					</motion.div>
+				</div>
+			</div>
+		</section>
+		<div className="w-full h-0.5 bg-sky-50"></div>
+	</div>
 );
 
 export default Entry;
--- src/components/HappyPlace.tsx
+++ src/components/HappyPlace.tsx
@@ -5,9 +5,9 @@ import SectionWrapper from "./Section";
 
 const HappyPlace: React.FC<{ id: string }> = ({ id }) => {
 	return (
-		<div>
+		<div className="flex-wrap justify-center">
 			<motion.div variants={fadeIn("right", "twen", 0.35, 0.5)}>
-				<div className="flex-wrap justify-center">
+				<div>
 					<motion.div variants={fadeIn("right", "twen", 0.35, 0.5)}>
 						<div className="rainbow-background flex justify-center space-x-10 sm:space-x-14 md:space-x-18 lg:space-x-24 xl:space-x-28 2xl:space-x-32 pt-6 pb-6">
 							<div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
@@ -47,8 +47,8 @@ const HappyPlace: React.FC<{ id: string }> = ({ id }) => {
 							</div>
 						</div>
 					</motion.div>
-					<div className="w-full text-center mt-32 text-cyan-300 font-bold">
-						<p className="text-base text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl text-cyan-300 font-bold lg:max-w-full border-2 border-white pt-6 pb-6">
+					<div className="w-full text-center justify-center text-cyan-300 font-bold  mt-20 sm:mt-24 md:mt-24 lg:mt-24 xl:mt-24 2-xl:mt-28">
+						<p className="text-base text-2xl text-center sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl text-cyan-300 font-bold lg:max-w-full border-2 border-white sm:pb-2 sm:pt-2 md:pb-2 md:pt-2 lg:pb-3 lg:pt-3 xl:pb-4 xl:pt-4">
 							HAPPY BIRTHDAY YOU PUNK!
 						</p>
 					</div>
@@ -60,7 +60,7 @@ const HappyPlace: React.FC<{ id: string }> = ({ id }) => {
 
 const HappyBirthdaySection = () => {
 	return (
-		<div className="h-[85vh] mt-32 mb-48">
+		<div className="h-[85vh] sm:mt-14 md:mt-16 lg:mt-24 xl:mt-28 2xl:mt-32 sm:mb-28 md:mb-32 lg:mb-40 xl:mb-44 2xl:mb-48">
 			<SectionWrapper Component={HappyPlace} id="happy_birthday" />{" "}
 		</div>
 	);
